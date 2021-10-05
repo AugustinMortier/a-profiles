ff453b15080a76c9675e20832d48f99ef955c4a4
augustinm <augustinm@met.no>
2021-10-05 11:17:41 +0200

    update README

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ profiles.detect_clouds(zmin=300, thr_noise=5, thr_clouds=4, verbose=True)
 profiles.detect_pbl(zmin=100, zmax=3000, under_clouds=True, verbose=True)
 
 #plot image
-profiles.plot(zref='agl', show_fog=True, show_clouds=True, show_pbl=True, log=True, vmin=1e1, vmax=1e5)
+#profiles.plot(zref='agl', show_fog=True, show_clouds=True, show_pbl=True, log=True, vmin=1e1, vmax=1e5)
 
 #plot single profile
 datetime = np.datetime64('2021-09-09T19:25:00')
-profiles.plot(datetime=datetime, vmax=1e5, zmax=12000, show_fog=True, show_clouds=True, show_pbl=True)
+profiles.plot(datetime=datetime, vmin=-1e4, vmax=5e4, zmax=12000, show_fog=True, show_clouds=True, show_pbl=True)
 
 ##produce rayleigh profile
 #altitude = profiles.data.altitude.data
