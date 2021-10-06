fix Rayleig calculation and start implementation of Klett method

aprofiles/plot/profile.py
@@ -144,13 +144,19 @@ def plot(da, datetime, var='attenuated_backscatter_0', zref='agl', zmin=None, zm
     if vmin!=None or vmax!=None:
         plt.xlim([vmin, vmax])
 
-    #set title and axis
+    #set title and axis labels
     latitude = da.station_latitude.data
     longitude = da.station_longitude.data
     altitude = da.station_altitude.data
     station_id = da.attrs['site_location']
+    #title
     plt.title('{} ({:.2f};{:.2f};{:.1f}m) - {}'.format(station_id, latitude, longitude, altitude, np.datetime_as_string(da_time[i_time]).split('.')[0]), weight='bold', fontsize=12)
-    plt.xlabel(da[var].long_name)
+    #labels
+    if 'units' in list(da[var].attrs) and da[var].units!=None:
+        xlabel = '{} ({})'.format(da[var].long_name, da[var].units)
+    else:
+        xlabel = '{}'.format(da[var].long_name)
+    plt.xlabel(xlabel)
     plt.ylabel('Altitude {} (m)'.format(zref.upper()))
 
     #add legend

aprofiles/profilesdata.py
@@ -98,7 +98,7 @@ class ProfilesData:
                 gates = np.arange(i-step,i+step)
                 indexes = [i for i in gates if i>0 and i<len(array)]
                 mean = np.nanmean(array[indexes])
-                std = np.nanstd(array[indexes], ddof=0)
+                std = np.nanstd(array[indexes])
                 if std!=0:
                     snr.append(mean/std)
                 else:
@@ -120,7 +120,7 @@ class ProfilesData:
             ),
             attrs=dict(
                 long_name="Signal to Noise Ratio",
-                units="bool",
+                units=None,
                 step=step
             )
         )
@@ -232,6 +232,8 @@ class ProfilesData:
 
         #add attribute
         new_profiles_data.data[var].attrs['range correction']=True
+        #remove units
+        new_profiles_data.data[var].attrs['units']=None
         return new_profiles_data
 
 
@@ -336,7 +338,7 @@ class ProfilesData:
             def make_mask(length, indexes_where_True):
                 #length: int: length of the mask
                 #indexes_where_true: list
-                mask = np.array([False for x in np.ones(length)])
+                mask = np.asarray([False for x in np.ones(length)])
                 mask[indexes_where_True] = len(indexes_where_True)*[True]
                 return mask
             
@@ -433,11 +435,11 @@ class ProfilesData:
             
 
             #7. find tops of clouds layers
-            tops = np.array([False for x in np.ones(len(data))])
+            tops = np.asarray([False for x in np.ones(len(data))])
             # conditions: look for bases above i_peaks[i], and data[top[i]] <= data[base[i]]
             for i in range(len(i_bases)):
-                mask_value = np.array(data<data[i_bases[i]])
-                mask_altitude = np.array([False for x in np.ones(len(data))])
+                mask_value = np.asarray(data<data[i_bases[i]])
+                mask_altitude = np.asarray([False for x in np.ones(len(data))])
                 mask_altitude[i_bases[i]:] = True
                 #the top is the first value that corresponds to the intersection of the two masks
                 cross_mask = np.logical_and(mask_value, mask_altitude)
@@ -543,12 +545,11 @@ class ProfilesData:
         dt_s = self._get_resolution('time')
         #number of timestamps to be to averaged
         nt_avg = max([1,round(t_avg/dt_s)])
-        rcs = rcs.rolling(time=nt_avg, min_periods=1, center=True).median()
+        rcs_data = rcs.rolling(time=nt_avg, min_periods=1, center=True).median().data
 
         clouds_bases, clouds_peaks, clouds_tops = [], [], []
         for i in (tqdm(range(len(self.data.time.data))) if verbose else range(len(self.data.time.data))):
-            data = rcs.data[i,:]
-            clouds = _detect_clouds_from_rcs(data, zmin, thr_noise, thr_clouds, min_snr)
+            clouds = _detect_clouds_from_rcs(rcs_data[i,:], zmin, thr_noise, thr_clouds, min_snr)
             
             #store info in 2D array
             clouds_bases.append(clouds['bases'])
@@ -631,9 +632,8 @@ class ProfilesData:
                 return 0
 
 
-        def _detect_pbl_from_rcs(data, zmin, zmax, wav_width, lowest_cloud, min_snr):
-            #detect pbl from range corrected signal between zmin and zmax using convolution with a wavelet.
-            #if lowest clouds is not np.nan, looks for PBL below the given altitude.
+        def _detect_pbl_from_rcs(data, zmin, zmax, wav_width, min_snr):
+            #detect pbl from range corrected signal between zmin and zmax using convolution with a wavelet..
 
             from scipy import signal
 
@@ -648,7 +648,7 @@ class ProfilesData:
             #the PBL is the maximum of the convolution
             #sets to nan outside of PBL search range
             convolution[0:self._get_index_from_altitude_AGL(zmin):] = np.nan
-            convolution[self._get_index_from_altitude_AGL(np.nanmin([zmax, lowest_cloud])):] = np.nan
+            convolution[self._get_index_from_altitude_AGL(zmax):] = np.nan
             i_pbl = np.nanargmax(abs(convolution))
             
             #calculates SNR
@@ -666,7 +666,7 @@ class ProfilesData:
         dt_s = self._get_resolution('time')
         #number of timestamps to be averaged
         nt_avg = max([1,round(t_avg/dt_s)])
-        rcs = rcs.rolling(time=nt_avg, min_periods=1, center=True).median()
+        rcs_data = rcs.rolling(time=nt_avg, min_periods=1, center=True).median().data
 
 
         #if under_clouds, check if clouds_bases is available
@@ -682,9 +682,8 @@ class ProfilesData:
 
         pbl = []
         for i in (tqdm(range(len(self.data.time.data))) if verbose else range(len(self.data.time.data))):
-            data = rcs.data[i,:]
             lowest_cloud_agl = lowest_clouds[i] - self.data.station_altitude.data
-            pbl.append(_detect_pbl_from_rcs(data, zmin, zmax, wav_width, lowest_cloud_agl, min_snr))
+            pbl.append(_detect_pbl_from_rcs(rcs_data[i,:], zmin, np.nanmin([zmax, lowest_cloud_agl]), wav_width, min_snr))
 
          #creates dataarrays
         self.data["pbl"] = xr.DataArray(
@@ -699,6 +698,162 @@ class ProfilesData:
                 time_avg=time_avg,
                 zmin=zmin,
                 zmax=zmax
+            )
+        )
+    
+    def klett_inversion(self, time_avg=1, zmin=4000., zmax=6000., under_clouds=True, method='backward', apriori={'lr': 50.}, verbose=False):
+        """Klett inversion using an apriori.
+
+        Args:
+            time_avg (int, optional): in minutes, the time during which we aggregate the profiles before detecting the PBL. Defaults to 1.
+            zmin (float, optional): minimum altitude AGL, in m, for looking for the initialization altitude. Defaults to 4000.
+            zmax (float, optional): maximum altitude AGL, in m, for looking for the initialization altitude. Defaults to 6000.
+            under_clouds (bool, optional): If True, and if clouds detection have been called before, force the initialization altitude to be found below the first cloud detected in the profile.
+            method (string, optional). 'backward' or 'forward. Defaults to 'forward'.
+            apriori (dict, optional). A Priori value to be used to constrain the inversion. Valid keys: 'lr' (Lidar Ratio, in sr) and 'aod' (unitless). Defaults to {'lr': 50}
+            verbose (bool, optional): verbose mode. Defaults to False.
+        
+        Returns:
+            :class: `ProfilesData object` with additional data array 'ext'.
+        """
+
+        def _iref(data, imin, imax):
+            #function that returns best index to be used for initializing the Klett inversion
+    
+            #it is important to copy the data not to modify it outside of the function
+            import copy
+            data = data.copy()
+
+            if imin<imax:
+                #limit from imin to imax
+                maxdata = np.nanmax(data)
+                data[0:imin] = maxdata
+                data[imax:] = maxdata
+                
+                #running average
+                from scipy.ndimage.filters import uniform_filter1d
+                avg_data = uniform_filter1d(data, size=3)
+
+                #get minimum from the averaged signal
+                ilow = np.nanargmin(avg_data)
+
+                #around minimum, get index of closest signal to average signal
+                n_around_min = 3
+                iclose = np.nanargmin(abs(data[ilow-n_around_min:ilow+n_around_min] - avg_data[ilow-n_around_min:ilow+n_around_min]))
+                
+                return ilow+iclose
+            else:
+                return None
+
+        def klett(data, iref, method, apriori, rayleigh):
+            #returns array of extinction in km-1
+            #rayleigh: aprofiles.rayleigh.Rayleigh object
+
+            if iref!=None:
+                if 'aod' in apriori:
+                    #search by dichotomy the LR that matches the apriori aod
+                    raise NotImplementedError('AOD apriori is not implemented yet')
+                    lr_aer = 50
+                else:
+                    #if we assume the LR, no need to minimize for matching aod 
+                    lr_aer = apriori['lr']
+            
+                import math
+                lr_mol = 8.*math.pi/3.
+                    
+                #calculation
+                if method=='backward':
+                    
+                    #vertical resolution
+                    dz = min(np.diff(rayleigh.altitude))
+
+                    int1_a = np.cumsum((lr_aer-lr_mol)*rayleigh.backscatter[:iref]*dz)
+                    int1_b = [2*int1_a[-1] - 2*int1_a[i] for i in range(iref)]
+                    phi = [np.log(abs(data[i]/data[iref])) + int1_b[i] for i in range(iref)]
+
+                    int2_a = 2*np.nancumsum(lr_aer*np.exp(phi)*dz)
+                    int2_b = [int2_a[-1] - int2_a[i] for i in range(iref)]
+
+                    #initialize total backscatter
+                    back_aer_iref = 0 #m-1
+                    beta_tot_iref = rayleigh.backscatter[iref] + back_aer_iref
+
+                    #total backscatter
+                    beta_tot = [np.exp(phi[i])/((1/beta_tot_iref)+int2_b[i]) for i in range(iref)]
+                    #aerosol backsatter (m-1.sr-1)
+                    beta_aer = beta_tot - rayleigh.backscatter[:iref]
+                    #aerosol extinction (m-1)
+                    sigma_aer=lr_aer*beta_aer
+                    #returns extinction in km-1 when valid, and np.nan elsewhere
+                    ext = [sigma_aer[i]*1e3 if i<len(sigma_aer) else np.nan for i in range(len(data))]
+            else:
+                ext = [np.nan for i in range(len(data))]
+
+            return ext
+
+        #we work on profiles averaged in time to reduce the noise
+        rcs = self.data.attenuated_backscatter_0
+        t_avg = time_avg * 60 #s
+        #time resolution in profiles data
+        dt_s = self._get_resolution('time')
+        #number of timestamps to be averaged
+        nt_avg = max([1,round(t_avg/dt_s)])
+
+        #if clouds detected, set to nan profiles where cloud is found below 4000m
+        lowest_clouds = self._get_lowest_clouds()
+        for i in range(len(self.data.time.data)):
+            if lowest_clouds[i]<=4000:
+                rcs.data[i,:] = [np.nan for _ in rcs.data[i,:]]
+
+        #average profiles
+        rcs_data = rcs.rolling(time=nt_avg, min_periods=1, center=True).median().data
+
+
+        #if under_clouds, check if clouds_bases is available
+        if under_clouds and 'clouds_bases' in list(self.data.data_vars):
+            lowest_clouds = self._get_lowest_clouds()
+        elif under_clouds and not 'clouds_bases' in list(self.data.data_vars):
+            import warnings
+            warnings.warn("under_clouds parameter sets to True (defaults value) when the clouds detection has not been applied to ProfileData object.")
+            lowest_clouds = [np.nan for i in np.arange(len(self.data.time))]
+        else:
+            lowest_clouds = [np.nan for i in np.arange(len(self.data.time))]
+        
+        #aerosol retrieval requires a molecular profile
+        altitude = self.data.altitude.data
+        wavelength = self.data.l0_wavelength.data
+        rayleigh = apro.rayleigh.Rayleigh(altitude, T0=298, P0=1013, wavelength=wavelength);
+
+        #aerosol inversion
+        ext = []
+        for i in (tqdm(range(len(self.data.time.data))) if verbose else range(len(self.data.time.data))):
+
+            #reference altitude
+            lowest_cloud_agl = lowest_clouds[i] - self.data.station_altitude.data
+            imin = self._get_index_from_altitude_AGL(zmin)
+            imax = self._get_index_from_altitude_AGL(np.min([zmax, lowest_cloud_agl]))
+            iref = _iref(rcs_data[i,:], imin, imax)
+
+            #klett inversion
+            ext.append(klett(rcs_data[i,:], iref, method, apriori, rayleigh))
+
+
+        #creates dataarrays
+        self.data["ext"] = xr.DataArray(
+            data=ext,
+            dims=["time", "altitude"],
+            coords=dict(
+                time=self.data.time.data,
+                altitude=self.data.altitude.data
+            ),
+            attrs=dict(
+                long_name="Extinction Coefficient",
+                method="{} Klett".format(method.capitalize()),
+                units="km-1",
+                time_avg=time_avg,
+                zmin=zmin,
+                zmax=zmax,
+                apriori=apriori
             )
         )
 

aprofiles/rayleigh.py
@@ -11,13 +11,13 @@ class Rayleigh:
     """Class for computing a standard rayleigh profile (backscatter and extinction)
 
     Attributes:
-        altitude (array): array of altitude to be used to compute rayleigh profile, in m.
+        altitude (array): array of altitude ASL to be used to compute the rayleigh profile, in m.
+        wavelength (float): Wavelength of the Rayleigh profile to be computed, in nm.
         T0 (float): Temperature at ground level, in K.
         P0 (float): Pressure at ground level, in hPa.
-        wavelength (float): Wavelength of the Rayleigh profile to be computed, in nm.
     """
 
-    def __init__(self, altitude=None, T0=298, P0=1013, wavelength=None):
+    def __init__(self, altitude, wavelength, T0=298, P0=1013):
         self.altitude = altitude
         self.T0 = T0
         self.P0 = P0
@@ -30,13 +30,29 @@ class Rayleigh:
         """Function that returns Rayleigh backscatter and extinction profiles for a standar atmosphere at a given wavelength.
 
         Returns:
-            self.bmol: Rayleigh backscatter coefficient (km-1.sr-1) at given wavelength for a standard atmosphere.
-            self.amol: Rayleigh extinction coefficient (km-1) at given wavelength for a standard atmosphere.
-        """        
+            self.backscatter: Rayleigh backscatter coefficient (m-1.sr-1) at given wavelength for a standard atmosphere.
+            self.extinction: Rayleigh extinction coefficient (m-1) at given wavelength for a standard atmosphere.
+        """
+
+        def _refi_air(wavelength):
+            """Function that calculates the refractive index of the air at a given wavelength in a standard atmosphere.
+            This is adapted from Peek and Reeder, 1972.
+
+            Args:
+                wavelength (float): wavelength, in µm.
+
+            Returns:
+                refractive index of the air.
+            """            
+            #wav (float): wavelength in µm
+            #returns refractive index of the air in standard atmosphere (Peck and Reeder)
+            var = 8060.51 + 2480990/(132.274-wavelength**-2) + 17455.7/(39.32957-wavelength**-2)
+            return var*1e-8+1
 
         #standard values at sea level
-        T0 = 298
-        P0 = 1013
+        T0 = 298 #K
+        P0 = 1013 *1e2 #Pa
+        p_He = 8;
                 
         #standard gradients & parameters
         atmo = {
@@ -57,12 +73,8 @@ class Rayleigh:
             }
         }
 
-        p_He = 8;
-
-        #convert pressure from hPa to Pa
-        p_z0 = P0*100;
         #convert altitude to km
-        z = self.altitude/1000;
+        z = self.altitude/1000
 
         #vertical resolution
         dz = min(z[1:]-z[0:-1])
@@ -79,40 +91,58 @@ class Rayleigh:
         z_Tz = np.arange(0,100,dz)
 
         #pressure profile
-        Pz=p_z0*np.exp(-z_Tz/p_He);
+        Pz=P0*np.exp(-z_Tz/p_He);
 
-        #Number of molecules
-        N_m=np.divide(Pz,(8.314/np.multiply(6.023e23,Tz)));
-        #molecular section
-        section_m=5.45*np.power(550/self.wavelength,4.09)*1e-32;
+        #molecules density and cross section
+        Ns = (6.02214e23/22.4141)/1000 #molecules.cm-3
+        #density (cm-3)
+        N_m = [Ns * (T0/P0)* (Pz[i]/Tz[i]) for i in range(len(Pz))]
 
-        #backscatter profile
-        bmol=N_m*section_m;
+        #cross section, in cm-2 (adapted from Bodhaine et al., 1999)
+        king_factor = 1.05 #tomasi et al., 2005
+        num = 24*(math.pi**3)*((_refi_air(self.wavelength*1e-3)**2-1)**2)
+        denum = ((self.wavelength*1e-7)**4)*(Ns**2)*((_refi_air(self.wavelength*1e-3)**2+2)**2)
+        section_m = (num/denum)*king_factor
+
         #extinction profile
-        amol=8.0*math.pi*bmol/3.0;
+        amol = N_m*np.array(section_m)
+        #backscatter profile
+        lr_mol = 8*math.pi/3;
+        bmol = amol / lr_mol
 
         #colocate vertically to input altitude
         imin = np.argmin(np.abs(np.array(z_Tz)-z[0]))
         imax = imin+len(z)
 
-        self.bmol = bmol[imin:imax]
-        self.amol = amol[imin:imax]
+        #output
+        self.cross_section = section_m # in cm-2
+        self.backscatter = bmol[imin:imax]*1e2 #from cm-1 to m-1
+        self.extinction = amol[imin:imax]*1e2 # from cm-1 to m-1
         
         return self
 
 def _main():
     import aprofiles as apro
     import matplotlib.pyplot as plt
-    path = "data/e-profile/2021/09/08/L2_0-20000-006735_A20210908.nc"
+    
+    #path = "examples/data/L2_0-20000-006735_A20210908.nc"
+    path = "examples/data/L2_0-20000-001492_A20210909.nc"
     apro_reader = apro.reader.ReadProfiles(path)
     profiles = apro_reader.read()
 
     altitude = profiles.data.altitude.data
     wavelength = profiles.data.l0_wavelength.data
 
-    T0=298; p_z0=1013;
-    rayleigh = Rayleigh(altitude,T0,p_z0,wavelength);
-    plt.plot(rayleigh.amol,altitude)
+    rayleigh = Rayleigh(altitude,wavelength);
+
+    #plot
+    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+    plt.plot(rayleigh.extinction,altitude)
+    plt.text(0.95, 0.94, r'$\sigma_m: {:.2e} cm-2$'.format(rayleigh.cross_section), horizontalalignment='right', verticalalignment='center', transform=ax.transAxes)
+    plt.title('Rayleigh Profile in a Standard Atmosphere ({}hPa, {}K)'.format(rayleigh.P0, rayleigh.T0), weight='bold')
+    plt.xlabel('Extinction coefficient @ {}nm (m-1)'.format(rayleigh.wavelength))
+    plt.ylabel('Altitude ASL (m)')
+    plt.tight_layout()
     plt.show()
 
 if __name__ == '__main__':