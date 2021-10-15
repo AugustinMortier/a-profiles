62b8b753110b4f3376a1f0e31f761413709c4f7b
augustinm <augustinm@met.no>
2021-10-15 08:52:39 +0200

    fix tests

--- tests/test_read_eprofile.py
+++ tests/test_read_eprofile.py
@@ -1,10 +1,10 @@
 import pytest
-from aprofiles import reader, read_eprofile
+from aprofiles.io import read_eprofile
 import xarray as xr
  
 def test__ReadEPROFILE():
     """Check if the reading the fie returns a Dataset"""
     path = "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
-    data = reader._ReadL2EPROFILE(path).read_file()
+    data = ReadEPROFILE(path).read()
     assert type(data) is xr.core.dataset.Dataset
 
