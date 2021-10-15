1d369111e070ddd368198f4dc9564cc50afc51bb
augustinm <augustinm@met.no>
2021-10-15 09:56:52 +0200

    fix tests

--- tests/test_read_eprofile.py
+++ tests/test_read_eprofile.py
@@ -4,11 +4,14 @@ import xarray as xr
  
 class TestReadEPROFILE:
 
-    def test__check_path(self):
-        assert type(self.path) is str    
+    def __init__(self):
+        self.path = "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
+
+    def test_read(self):
+        data = ReadEPROFILE(self.path).read()
+        assert type(data) is xr.core.dataset.Dataset 
     
     #"""Check if the reading the fie returns a Dataset"""
     #path = "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
     #data = ReadEPROFILE(path).read()
     #assert type(data) is xr.core.dataset.Dataset
-
