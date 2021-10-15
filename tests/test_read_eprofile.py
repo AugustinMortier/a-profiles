e07e4148fff573cde5808b45eecaf455747e34ba
augustinm <augustinm@met.no>
2021-10-15 10:08:03 +0200

    work on test

--- tests/test_read_eprofile.py
+++ tests/test_read_eprofile.py
@@ -7,9 +7,14 @@ import xarray as xr
 def path():
     return "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
 
-def test_read(path):
-    data = read_eprofile.ReadEPROFILE(path).read()
-    assert type(data) is xr.core.dataset.Dataset 
+class TestReadEPROFILE:
+
+    def __init__(self):
+        self.path = path
+
+    def test_read(self):
+        data = read_eprofile.ReadEPROFILE(self.path).read()
+        assert type(data) is xr.core.dataset.Dataset 
 
 #"""Check if the reading the fie returns a Dataset"""
 #path = "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
