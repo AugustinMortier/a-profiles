7ef9e6da594bb9ed31b9a1e3c7bd7c30973cc99f
augustinm <augustinm@met.no>
2021-10-21 09:52:20 +0200

    work on git workflow

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,6 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-import aprofiles
 
 
 # -- Project information -----------------------------------------------------
