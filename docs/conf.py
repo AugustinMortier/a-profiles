09133adf4b8a311fdbd6a15f8dc357b55fee96b1
augustinm <augustinm@met.no>
2021-10-22 21:01:56 +0200

    clean up conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -85,5 +85,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#some options
+# Some options
 add_module_names = True
