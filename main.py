f7d05fca06ad1ea99043e05e0dde6489c0aad31b
augustinm <augustinm@met.no>
2021-10-22 21:03:58 +0200

    clean up main.py

--- main.py
+++ main.py
@@ -4,70 +4,70 @@
 # @email augustinm@met.no
 # @desc A-Profiles - Main
 
-import aprofiles as apro
+import time
+
 import numpy as np
 
-import time
+import aprofiles as apro
+
 start_time = time.time()
 
-#read some data
+# read some data
 path = "examples/data/E-PROFILE/L2_0-20000-006735_A20210908.nc"
 path = "examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc"
-
 
 apro_reader = apro.reader.ReadProfiles(path)
 profiles = apro_reader.read()
 
-#signal to noise ratio
-#profiles.snr(verbose=True)
-#profiles.plot(var='snr',vmin=0, vmax=3, cmap='Greys_r')
+# signal to noise ratio
+# profiles.snr(verbose=True)
+# profiles.plot(var='snr',vmin=0, vmax=3, cmap='Greys_r')
 
-#saturation
-#profiles.plot(zmax=5000., log=True, vmin=1e-2, vmax=1e1)
-#profiles.desaturate_below(z=4000., inplace=True)
-#profiles.plot(zmax=5000., log=True, vmin=1e-2, vmax=1e1)
+# saturation
+# profiles.plot(zmax=5000., log=True, vmin=1e-2, vmax=1e1)
+# profiles.desaturate_below(z=4000., inplace=True)
+# profiles.plot(zmax=5000., log=True, vmin=1e-2, vmax=1e1)
 
-#gaussian filter
-#profiles.plot(log=True, vmin=1e-2, vmax=1e1)
-#profiles.gaussian_filter(sigma=0.5, inplace=True)
-#profiles.plot(log=True, vmin=1e-2, vmax=1e1)
+# gaussian filter
+# profiles.plot(log=True, vmin=1e-2, vmax=1e1)
+# profiles.gaussian_filter(sigma=0.5, inplace=True)
+# profiles.plot(log=True, vmin=1e-2, vmax=1e1)
 
-#extrapolation lowest layers
-#profiles.plot(zmax=1000., log=True, vmin=1e-2, vmax=1e1)
+# extrapolation lowest layers
+# profiles.plot(zmax=1000., log=True, vmin=1e-2, vmax=1e1)
 profiles.extrapolate_below(z=150, inplace=True)
-#profiles.plot(zmax=1000., log=True, vmin=1e-2, vmax=1e1)
+# profiles.plot(zmax=1000., log=True, vmin=1e-2, vmax=1e1)
 
-#detection
+# detection
 profiles.foc(zmin_cloud=200)
 profiles.clouds(zmin=300, thr_noise=5, thr_clouds=4, verbose=True)
 profiles.pbl(zmin=200, zmax=3000, under_clouds=False, min_snr=2., verbose=True)
 
-#plot image
-#profiles.plot(zref='agl', zmax=6000., show_foc=True, show_clouds=True, show_pbl=True, log=True, vmin=1e-2, vmax=1e1)
+# plot image
+# profiles.plot(zref='agl', zmax=6000., show_foc=True, show_clouds=True, show_pbl=True, log=True, vmin=1e-2, vmax=1e1)
 
-#plot single profile
-#datetime = np.datetime64('2021-09-09T19:25:00')
-#datetime = np.datetime64('2021-09-09T21:20:00')
-#datetime = np.datetime64('2021-09-09T10:25:00')
-#profiles.plot(datetime=datetime, vmin=-1, vmax=10, zmax=12000, show_fog=True, show_clouds=True, show_pbl=True)
+# plot single profile
+# datetime = np.datetime64('2021-09-09T19:25:00')
+# datetime = np.datetime64('2021-09-09T21:20:00')
+# datetime = np.datetime64('2021-09-09T10:25:00')
+# profiles.plot(datetime=datetime, vmin=-1, vmax=10, zmax=12000, show_fog=True, show_clouds=True, show_pbl=True)
 
-#aerosol inversion
+# aerosol inversion
 
-#backward
-#profiles.inversion(zmin=4000, zmax=6000, remove_outliers=False, method='backward', verbose=True)
-#profiles.plot(var='ext', zmax=6000, vmin=0, vmax=5e-2)
-#profiles.plot(var='aod')
+# backward
+# profiles.inversion(zmin=4000, zmax=6000, remove_outliers=False, method='backward', verbose=True)
+# profiles.plot(var='ext', zmax=6000, vmin=0, vmax=5e-2)
+# profiles.plot(var='aod')
 
-#forward
+# forward
 profiles.inversion(zmin=4000, zmax=6000, remove_outliers=True, method='forward', verbose=True)
 profiles.plot(var='extinction', zmax=6000, vmin=0, vmax=5e-2)
 profiles.plot(var='mass_concentration:dust', zmax=6000, vmin=0, vmax=5e-2)
-#profiles.plot('pbl', ymin=0, ymax=3000)
+# profiles.plot('pbl', ymin=0, ymax=3000)
 
-##produce rayleigh profile
-#altitude = profiles.data.altitude.data
-#wavelength = profiles.data.l0_wavelength.data
-#rayleigh = apro.rayleigh_data.RayleighData(altitude, wavelength=wavelength, T0=298, P0=1013);
-#rayleigh.plot()
+# # produce rayleigh profile
+# altitude = profiles.data.altitude.data
+# wavelength = profiles.data.l0_wavelength.data
+# rayleigh = apro.rayleigh_data.RayleighData(altitude, wavelength=wavelength, T0=298, P0=1013);
+# rayleigh.plot()
 print("--- %s seconds ---" % (time.time() - start_time))
-
