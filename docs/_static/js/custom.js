1d67ce0657b969cd9f20d10d3bf42334a01ea46d
augustinm <augustinm@met.no>
2021-11-23 11:43:51 +0100

    add doc images to static directory

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -4,7 +4,7 @@ $(function() {
     // add V-Profiles in top right icon links
     var addInList = '<li class="nav-item">'
         addInList += '<a class="nav-link" href="https://vprofiles.met.no" rel="noopener" target="_blank" title="V-Profiles">'
-            addInList += '<span><img src="_images/vprofiles.ico" height="20" width="20"></span>'
+            addInList += '<span><img src="_static/images/vprofiles.ico" height="20" width="20"></span>'
         addInList += '</a>'
     addInList += '</li>'
     document.getElementById('navbar-icon-links').innerHTML += addInList
