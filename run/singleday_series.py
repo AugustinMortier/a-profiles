fd6cf8b519699c68fd10f203e6debf94900c0c6d
augustinm <augustinm@met.no>
2021-11-29 17:27:01 +0100

    add CLI

--- run/singleday_series.py
+++ run/singleday_series.py
@@ -11,53 +11,48 @@ import utils
 BASE_DIR_IN = 'data/e-profile'
 BASE_DIR_OUT = 'data/v-profiles'
 
-def _main(date: array, instrument_types=['CHM15k', 'mini-MPL']):
+def _main(date, instrument_types=['CHM15k', 'mini-MPL']):
 
-    # transforms dates from strin to array
-    if isinstance(dates, str):
-        dates = [dates]
+    yyyy = date.split('-')[0]
+    mm = date.split('-')[1]
+    dd = date.split('-')[2]
 
-    for date in dates:
-        yyyy = date.split('-')[0]
-        mm = date.split('-')[1]
-        dd = date.split('-')[2]
+    # list all files in in directory
+    datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
+    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+    
+    # data processing
+    for i in (tqdm(range(len(onlyfiles)), desc=date)):
+        path = os.path.join(datepath, onlyfiles[i])
+        utils.workflow.workflow(path, instrument_types, BASE_DIR_OUT, verbose=False)
+    
+    # list all files in out directory
+    datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
+    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+    
+    # create calendar
+    calname = f"{yyyy}-{mm}-cal.json"
+    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
+        utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
 
-        # list all files in in directory
-        datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
-        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-        
-        # data processing
-        for i in (tqdm(range(len(onlyfiles)), desc=date)):
-            path = os.path.join(datepath, onlyfiles[i])
-            utils.workflow.workflow(path, instrument_types, BASE_DIR_OUT, verbose=False)
-        
-        # list all files in out directory
-        datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
-        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-        
-        # create calendar
-        calname = f"{yyyy}-{mm}-cal.json"
-        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
-            utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
+    # add to calendar
+    for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
+        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+        utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
 
-        # add to calendar
-        for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
-            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-            utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
+    # create map
+    mapname = f"{yyyy}-{mm}-map.json"
+    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
+        utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
 
-        # create map
-        mapname = f"{yyyy}-{mm}-map.json"
-        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
-            utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
-
-        # add to map
-        for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
-            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-            utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
+    # add to map
+    for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
+        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+        utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
     
 if __name__ == "__main__":
-    import sys
+    import argparse
     if len(sys.argv)>1:
         _main(sys.argv[1])
     else:
-        _main('2021-09-10')
+        _main('2021-09-09')
