f02ca6ddf49c27103762ef724904e6018eed9314
augustinm <augustinm@met.no>
2021-11-29 13:16:36 +0100

    fix imports in multithread processing

--- run/singleday_multithread.py
+++ run/singleday_multithread.py
@@ -7,7 +7,7 @@ import os
 
 from tqdm import tqdm
 
-import run
+import utils
 
 BASE_DIR_IN = 'data/e-profile'
 BASE_DIR_OUT = 'data/v-profiles'
@@ -25,7 +25,7 @@ def _main(date: str, instrument_types=['CHM15k', 'mini-MPL']):
     # data processing
     with tqdm(total=len(onlyfiles), desc=date) as pbar:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(run.workflow.workflow, path=os.path.join(datepath, filename), instrument_types=instrument_types, base_dir=BASE_DIR_OUT, verbose=False) for filename in onlyfiles]
+            futures = [executor.submit(utils.workflow.workflow, path=os.path.join(datepath, filename), instrument_types=instrument_types, base_dir=BASE_DIR_OUT, verbose=False) for filename in onlyfiles]
             for future in concurrent.futures.as_completed(futures):
                 pbar.update(1)
 
@@ -36,22 +36,22 @@ def _main(date: str, instrument_types=['CHM15k', 'mini-MPL']):
     # create calendar
     calname = f"{yyyy}-{mm}-cal.json"
     if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
-        run.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
+        utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
 
     # add to calendar
     for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
         fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        run.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
+        utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
 
     # create map
     mapname = f"{yyyy}-{mm}-map.json"
     if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
-        run.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
+        utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
 
     # add to map
     for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
         fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        run.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
+        utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
     
 if __name__ == "__main__":
     import sys
