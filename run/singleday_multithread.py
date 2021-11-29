f8e82bea829ab55421f74425da37962c29961cb0
augustinm <augustinm@met.no>
2021-11-29 13:34:41 +0100

    allows to process multiple days

--- run/singleday_multithread.py
+++ run/singleday_multithread.py
@@ -12,46 +12,51 @@ import utils
 BASE_DIR_IN = 'data/e-profile'
 BASE_DIR_OUT = 'data/v-profiles'
 
-def _main(date: str, instrument_types=['CHM15k', 'mini-MPL']):
+def _main(dates: array, instrument_types=['CHM15k', 'mini-MPL']):
 
-    yyyy = date.split('-')[0]
-    mm = date.split('-')[1]
-    dd = date.split('-')[2]
+    # transforms dates from strin to array
+    if isinstance(dates, str):
+        dates = [dates]
 
-    # list all files in in directory
-    datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
-    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-    
-    # data processing
-    with tqdm(total=len(onlyfiles), desc=date) as pbar:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(utils.workflow.workflow, path=os.path.join(datepath, filename), instrument_types=instrument_types, base_dir=BASE_DIR_OUT, verbose=False) for filename in onlyfiles]
-            for future in concurrent.futures.as_completed(futures):
-                pbar.update(1)
+    for date in dates:
+        yyyy = date.split('-')[0]
+        mm = date.split('-')[1]
+        dd = date.split('-')[2]
 
-    # list all files in out directory
-    datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
-    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-    
-    # create calendar
-    calname = f"{yyyy}-{mm}-cal.json"
-    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
-        utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
+        # list all files in in directory
+        datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
+        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+        
+        # data processing
+        with tqdm(total=len(onlyfiles), desc=date) as pbar:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = [executor.submit(utils.workflow.workflow, path=os.path.join(datepath, filename), instrument_types=instrument_types, base_dir=BASE_DIR_OUT, verbose=False) for filename in onlyfiles]
+                for future in concurrent.futures.as_completed(futures):
+                    pbar.update(1)
 
-    # add to calendar
-    for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
-        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
+        # list all files in out directory
+        datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
+        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+        
+        # create calendar
+        calname = f"{yyyy}-{mm}-cal.json"
+        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
+            utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
 
-    # create map
-    mapname = f"{yyyy}-{mm}-map.json"
-    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
-        utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
+        # add to calendar
+        for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
+            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+            utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
 
-    # add to map
-    for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
-        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
+        # create map
+        mapname = f"{yyyy}-{mm}-map.json"
+        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
+            utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
+
+        # add to map
+        for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
+            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+            utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
     
 if __name__ == "__main__":
     import sys
--- run/singleday_series.py
+++ run/singleday_series.py
@@ -11,44 +11,49 @@ import utils
 BASE_DIR_IN = 'data/e-profile'
 BASE_DIR_OUT = 'data/v-profiles'
 
-def _main(date: str, instrument_types=['CHM15k', 'mini-MPL']):
+def _main(date: array, instrument_types=['CHM15k', 'mini-MPL']):
 
-    yyyy = date.split('-')[0]
-    mm = date.split('-')[1]
-    dd = date.split('-')[2]
+    # transforms dates from strin to array
+    if isinstance(dates, str):
+        dates = [dates]
 
-    # list all files in in directory
-    datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
-    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-    
-    # data processing
-    for i in (tqdm(range(len(onlyfiles)), desc=date)):
-        path = os.path.join(datepath, onlyfiles[i])
-        utils.workflow.workflow(path, instrument_types, BASE_DIR_OUT, verbose=False)
-    
-    # list all files in out directory
-    datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
-    onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
-    
-    # create calendar
-    calname = f"{yyyy}-{mm}-cal.json"
-    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
-        utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
+    for date in dates:
+        yyyy = date.split('-')[0]
+        mm = date.split('-')[1]
+        dd = date.split('-')[2]
 
-    # add to calendar
-    for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
-        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
+        # list all files in in directory
+        datepath = os.path.join(BASE_DIR_IN, yyyy, mm, dd)
+        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+        
+        # data processing
+        for i in (tqdm(range(len(onlyfiles)), desc=date)):
+            path = os.path.join(datepath, onlyfiles[i])
+            utils.workflow.workflow(path, instrument_types, BASE_DIR_OUT, verbose=False)
+        
+        # list all files in out directory
+        datepath = os.path.join(BASE_DIR_OUT, yyyy, mm, dd)
+        onlyfiles = [f for f in os.listdir(datepath) if os.path.isfile(os.path.join(datepath, f))]
+        
+        # create calendar
+        calname = f"{yyyy}-{mm}-cal.json"
+        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, 'calendar', calname)):
+            utils.json_calendar.make_calendar(BASE_DIR_OUT, yyyy, mm, calname)
 
-    # create map
-    mapname = f"{yyyy}-{mm}-map.json"
-    if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
-        utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
+        # add to calendar
+        for i in (tqdm(range(len(onlyfiles)), desc='calendar  ')):
+            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+            utils.json_calendar.add_to_calendar(fn, BASE_DIR_OUT, yyyy, mm, dd, calname)
 
-    # add to map
-    for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
-        fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
-        utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
+        # create map
+        mapname = f"{yyyy}-{mm}-map.json"
+        if not os.path.exists(os.path.join(BASE_DIR_OUT, yyyy, mm, mapname)):
+            utils.json_map.make_map(BASE_DIR_OUT, yyyy, mm, mapname)
+
+        # add to map
+        for i in (tqdm(range(len(onlyfiles)), desc='map       ')):
+            fn = os.path.join(BASE_DIR_OUT, yyyy, mm, dd, onlyfiles[i])
+            utils.json_map.add_to_map(fn, BASE_DIR_OUT, yyyy, mm, dd, mapname)
     
 if __name__ == "__main__":
     import sys
