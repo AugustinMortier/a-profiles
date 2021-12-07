1ab582cf92850311ecc147ffe6bb829df9ad7213
augustinm <augustinm@met.no>
2021-12-07 21:49:20 +0100

    fix relative path of v-profiles icon

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -2,9 +2,19 @@ console.log('Yo')
 
 $(function() {
     // add V-Profiles in top right icon links
+
+    // get path
+    if (window.location.href.includes('build')){
+        // if local
+        var url = window.location.href.split('_build')[0]
+    } else if (window.location.href.includes('readthedocs.io')){
+        // if readthedocs
+        var url = window.location.href.split('latest')[0]+'latest/'
+    }
+
     var addInList = '<li class="nav-item">'
         addInList += '<a class="nav-link" href="https://vprofiles.met.no" rel="noopener" target="_blank" title="V-Profiles">'
-            addInList += '<span><img src="_static/images/vprofiles-bold.ico" height="20" width="20"></span>'
+            addInList += '<span><img src="'+url+'_static/images/vprofiles-bold.ico" height="20" width="20"></span>'
         addInList += '</a>'
     addInList += '</li>'
     document.getElementById('navbar-icon-links').innerHTML += addInList
