92c6bee12f80a000dea586e47a48a372c30a8195
augustinm <augustinm@met.no>
2021-12-09 13:34:51 +0100

    update docstring

--- aprofiles/simulation/ext2back.py
+++ aprofiles/simulation/ext2back.py
@@ -16,7 +16,7 @@ class ExtinctionToAttenuatedBackscatter:
     """Class for simulating measurements (attenuated backscatter profiles) for different models.
 
     Attributes:
-        - `model` ({'rayleigh', 'standard', 'aloft'}): atmospheric model to be simulated.
+        - `model` ({'null', 'step'}): atmospheric model to be simulated.
         - `wavelength` (float): Wavelength of the Rayleigh profile to be computed, in nm.
         - `lidar_ratio` (float): Lidar Ratio, in sr.
         - `noise` (float): Noise level. The noise is normalized to the maximum extinction value in the profile.
@@ -25,17 +25,17 @@ class ExtinctionToAttenuatedBackscatter:
             >>> # some imports
             >>> import aprofiles as apro
             >>> # simulate rayleigh profiles with a random noise
-            >>> simulator = apro.simulator.ExtinctionToAttenuatedBackscatter(model = 'rayleigh', wavelength = 1064., lidar_ratio = 50., noise = 0.5);
+            >>> simulator = apro.simulator.ExtinctionToAttenuatedBackscatter(model = 'step', wavelength = 1064., lidar_ratio = 50., noise = 0.5);
             # calls the to_profiles_data method
             sim_profiles = simulator.to_profiles_data()
             # plot modelled extinction
             sim_profiles.plot('extinction_model')
 
-            .. figure:: ../../docs/_static/images/simulation_rayleigh.png
+            .. figure:: ../../docs/_static/images/simulation_step-model.png
                 :scale: 80 %
-                :alt: rayleigh simulation
+                :alt: simlation step model
 
-                Simulation of pure Rayleigh profiles.
+                Simulation of aerosol extinction using a step model.
     """    
     # get the right reading class
     def __init__(self, model, wavelength, lidar_ratio, noise):
@@ -70,30 +70,26 @@ class ExtinctionToAttenuatedBackscatter:
         ).tolist()
         altitude = np.arange(15, 15000, 15)
 
-        # open molecular profile
-        rayleigh = RayleighData(altitude, self.wavelength, T0=298, P0=1013)
-
-        extinction = []
+        # extinction models
+        extinction_model = []
         # model clear: twice the molecular
-        if self.model == "empty":
+        if self.model == "null":
             extinction_profile = np.asarray([0 for z in altitude])
         if self.model == "step":
-            extinction_profile = np.asarray([0.1e-3 if z<3000 else 0 for z in altitude])
+            extinction_profile = np.asarray([0.1 if z<3000 else 0 for z in altitude])
         if self.model == "aloft":
             #TODO: gaussian in altitude (3 km)
             pass
 
-        print(np.shape(extinction_profile), np.shape(altitude))
-
         # use profile over the whole day
         for t in _time:
             norm_noise = self.noise*np.nanmax(extinction_profile) / altitude[-1]**2
-            noise_profile = [norm_noise * random.random() * z**2 for z in altitude]
-            extinction.append(extinction_profile + noise_profile)
+            noise_profile = [norm_noise * random.uniform(-1,1) * z**2 for z in altitude]
+            extinction_model.append(np.asarray(extinction_profile + noise_profile))
 
         ds = xr.Dataset(
             data_vars=dict(
-                extinction_model=(["time", "altitude"], extinction),
+                extinction_model=(["time", "altitude"], extinction_model),
                 station_altitude=(0.0),
                 station_latitude=(0.0),
                 station_longitude=(0.0),
@@ -104,7 +100,7 @@ class ExtinctionToAttenuatedBackscatter:
         )
         ds.extinction_model.attrs = {
             "long_name": f"Extinction Coefficient (model) @ {self.wavelength} nm",
-            "units": "m-1",
+            "units": "km-1",
         }
         ds.l0_wavelength.attrs = {
             "units": "nm"
@@ -123,7 +119,7 @@ class ExtinctionToAttenuatedBackscatter:
         # frequently used variables
         altitude = ds.altitude.data
         time = ds.time.data
-        extinction = ds.extinction_model.data
+        extinction_model = ds.extinction_model.data * 1e-3 #conversion from km-1 to m-1
         vertical_resolution = min(np.diff(altitude))
 
         # open molecular profile
@@ -132,17 +128,19 @@ class ExtinctionToAttenuatedBackscatter:
         # attenuated_backscatter calculation
         attenuated_backscatter = []
         for i, t in enumerate(time):
-            total_backscatter = rayleigh.backscatter + np.divide(extinction[i,:], self.lidar_ratio)
-            total_extinction = rayleigh.extinction + extinction[i]
-            optical_depth = np.cumsum(np.asarray(total_extinction) * vertical_resolution)
+            total_backscatter = rayleigh.backscatter + np.divide(extinction_model[i,:], self.lidar_ratio)
+            total_extinction = rayleigh.extinction + extinction_model[i]
+            optical_depth = np.cumsum(total_extinction * vertical_resolution)
             transmission = np.exp(-optical_depth)
-            attenuated_backscatter.append( [total_backscatter[j] * transmission[j]**2 for j, z in enumerate(altitude)])
+            attenuated_backscatter.append(np.asarray([total_backscatter[j] * (transmission[j]**2) for j, z in enumerate(altitude)]))
+        # convert list to np array
+        attenuated_backscatter = np.asarray(attenuated_backscatter)
 
         # add attenuated_backscatter as new dataarray
         ds["attenuated_backscatter_0"] = xr.DataArray(
-            data=np.multiply(attenuated_backscatter, 1e-6), #conversion from m-1.s-1 to µm-1.sr-1
+            data=attenuated_backscatter * 1e6, #conversion from m-1.sr-1 to Mm-1.sr-1
             dims=["time", "altitude"],
-            attrs=dict(long_name=f"Attenuated Backscatter @ {self.wavelength}nm", units='µm-1.sr-1'),
+            attrs=dict(long_name=f"Attenuated Backscatter @ {self.wavelength}nm", units='Mm-1.sr-1'),
         )
         return ds
 
