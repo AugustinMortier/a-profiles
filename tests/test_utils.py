ca1dd75a80d810b0ef72723920ac378f520014a5
augustinm <augustinm@met.no>
2022-01-26 13:57:43 +0100

    add some tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,9 @@ def test_get_true_indexes():
     true_indexes = apro.utils.get_true_indexes([True, False, True])
     # test values
     assert true_indexes == [0, 2]
+
+def test_make_mask():
+    mask = apro.utils.make_mask(5, [0, 2, 3])
+    # test values
+    assert len(mask) == 5
+    assert len([i for i in mask if i]) == 3    
