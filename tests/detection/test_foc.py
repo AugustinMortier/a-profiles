3999d3c508c13f8bb56f4b48a85f76a22d8631f0
augustinm <augustinm@met.no>
2022-01-27 11:41:23 +0100

    add some tests

--- tests/detection/test_foc.py
+++ tests/detection/test_foc.py
@@ -20,3 +20,18 @@ def test_detect_foc(subtime_profiles):
     # test values
     assert np.round(np.mean(foc), 4) == 0.2083
 
+def test__detect_fog_from_cloud_base_height(subtime_profiles):
+    # call method
+    zmin_cloud = 200.
+    foc = apro.detection.foc._detect_fog_from_cloud_base_height(subtime_profiles, zmin_cloud)
+    # test values
+    assert np.round(np.nanmean(foc), 3) == 0.208
+
+def test__detect_fog_snr(subtime_profiles):
+    # call method
+    z_snr = 2000.
+    var = "attenuated_backscatter_0"
+    min_snr = 2.
+    foc = apro.detection.foc._detect_fog_from_snr(subtime_profiles, z_snr, var, min_snr)
+    # test values
+    assert np.round(np.nanmean(foc), 3) == 0.167
