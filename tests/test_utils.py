4c69c50fdb864ab047e78f0d8d2d6fc1bb11c821
augustinm <augustinm@met.no>
2022-01-27 10:49:51 +0100

    add some tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from aprofiles import utils
 
 
@@ -12,7 +13,15 @@ def test_make_mask():
     assert len(mask) == 5
     assert len([i for i in mask if i]) == 3    
 
-def file_exists():
+def test_file_exists():
     check = utils.file_exists("examples/data/E-PROFILE/L2_0-20000-001492_A20210909.nc")
     # test values
     assert check == True
+
+def test_snr_at_iz():
+    array = np.linspace(0,10)
+    iz = 5
+    step = 2
+    snr = utils.snr_at_iz(array, iz, step)
+    # test values
+    assert np.round(snr, 5) == 4.02492
