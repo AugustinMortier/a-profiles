ed9466e014b7943029b2b7316a0d84025b2861fc
augustinm <augustinm@met.no>
2022-07-15 09:50:04 +0200

    rename key

--- cli/aprocess.py
+++ cli/aprocess.py
@@ -138,7 +138,7 @@ def main(
         # get station id from file name
         stations_id = ["-".join(onlyfile.split("/")[-1].split("AP_")[1].split("-", 5)[:5]) for onlyfile in onlyfiles]
         # exclude moving stations
-        stations_id = [station for station in stations_id if station not in CFG["exclude_stations_id"]]
+        stations_id = [station for station in stations_id if station not in CFG["exclude_stations_id_from_climatology"]]
         for station_id in tqdm(stations_id, desc="climatology", disable=disable_progress_bar):
             try:
                 utils.json_climatology.compute_climatology(basedir_out, station_id, variables="extinction", aerosols_only=True)
