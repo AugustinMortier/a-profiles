77342e8c5c351c0bff89370c5b09d9cbf947c095
augustinm <augustinm@met.no>
2025-02-10 16:45:54 +0100

    fix test

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -124,7 +124,7 @@ class TestProfilesData:
         subtime_profiles.foc(zmin_cloud=200)
         subtime_profiles.pbl(zmin=200, zmax=3000, under_clouds=True)
         # call plotting functions
-        fig1 = subtime_profiles.plot(show_foc=True, show_clouds=True, show_pbl=True, log=True, show_fig=False)
+        fig1 = subtime_profiles.plot(show_foc=True, show_clouds=True, show_pbl=True, vmin=1e-1, vmax=1e2, log=True, show_fig=False)
         fig2 = subtime_profiles.plot(datetime=np.datetime64('2021-09-09T21:20:00'), show_foc=True, show_clouds=True, show_pbl=True, show_fig=False)
         fig3 = subtime_profiles.plot(var='calibration_constant_0', show_fig=False)
         
